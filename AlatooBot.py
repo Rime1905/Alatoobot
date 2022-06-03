13308186478d938a4621de69ea1b73e91b5f564d
Rime1905 <99634100+Rime1905@users.noreply.github.com>
2022-06-03 23:32:53 +0600

    Update AlatooBot.py

--- AlatooBot.py
+++ AlatooBot.py
@@ -2,9 +2,9 @@ import discord
 from discord.utils import get
 from discord_components import Button, Select, SelectOption, ComponentsBot
 
-token = ('OTY3MDkyODg5MDI2OTY5NzAw.YmLRcA.uZiKdZdYOVpQ2ojoYNlFP3Oqfwk')
 
 bot = ComponentsBot("!")
+
 
 @bot.event
 async def on_ready():
@@ -13,18 +13,21 @@ async def on_ready():
 
 @bot.command()
 async def button(ctx):
-    await ctx.send("Buttons!", components=[Button(label="Button", custom_id="button1")])
+    await ctx.send("Placeholder!", components=[Button(label="Button", custom_id="button1")])
     interaction = await bot.wait_for(
         "button_click", check=lambda inter: inter.custom_id == "button1"
     )
     await interaction.send(content="Button Clicked")
 
+
 @bot.command()
-async def xd(ctx):
-    embedVar = discord.Embed(title="Python news", color=000000)
-    embedVar.set_image(url="https://img1.wallspic.com/crops/4/9/3/3/6/163394/163394-python_programer-python-programming_language-standing-source_code-3840x2160.png")
-    embedVar.add_field(name="----------------------------------------------", value="There will be news from r/Python (Python's Subreddit)", inline=True)
+async def links(ctx):
+    embedVar = discord.Embed(
+        title="List of Alatoo related links", color=000000)
+    embedVar.add_field(name="-------------------------------------------",
+                       value="http://alatoo.edu.kg/#gsc.tab=0\n https://www.instagram.com/alatoo.edu.kg/\nhttps://ocs.alatoo.edu.kg\nhttps://my.alatoo.edu.kg", inline=True)
     await ctx.send(embed=embedVar)
+
 
 @bot.command()
 async def select(ctx):
@@ -34,7 +37,8 @@ async def select(ctx):
             Select(
                 placeholder="Press this menu",
                 options=[
-                    SelectOption(label="Computer Science", value="СomputerScience"),
+                    SelectOption(label="Computer Science",
+                                 value="СomputerScience"),
                     SelectOption(label="Medicine", value="Medicine")
                 ],
                 custom_id="select1",
@@ -45,8 +49,18 @@ async def select(ctx):
     interaction = await bot.wait_for(
         "select_option", check=lambda inter: inter.custom_id == "select1"
     )
-    memberRole = discord.utils.get(ctx.guild.roles, name=f"{interaction.values[0]}")
+    memberRole = discord.utils.get(
+        ctx.guild.roles, name=f"{interaction.values[0]}")
     await interaction.send(content=f"{interaction.values[0]} selected. Role Acquired!")
     await ctx.author.add_roles(memberRole)
+
+
+@bot.command()
+async def help(ctx):
+    embedVar = discord.Embed(
+        title="List of commands", color=000000)
+    embedVar.add_field(name="-------------------------------------------",
+                       value="!button\n!links\n!select", inline=True)
+    await ctx.send(embed=embedVar)
 
 bot.run(token)
